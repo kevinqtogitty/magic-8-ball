f37dc5d0ad092eb1830a065f54127bf45b3614be
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-04 18:02:45 +0100

    added matcap material

---
+++ src/components/ChosenOption.tsx
@@ -0,0 +1,35 @@
+import { animated, useSpring } from '@react-spring/three';
+import { Float, Text3D } from '@react-three/drei';
+import { useTexture } from '@react-three/drei/core/useTexture';
+import { useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Mesh } from 'three';
+import useClicked from '../state/useClicked';
+import useOptions from '../state/useOptions';
+
+const ChosenOption: React.FC = () => {
+  const chosenOption = useOptions((state) => state.chosenOption);
+  const textRef = useRef<Mesh>(null!);
+
+  const matcap = useTexture('/matcaps/736655_D9D8D5_2F281F_B1AEAB.png');
+
+  return (
+    <Float>
+      <animated.mesh ref={textRef}>
+        <Text3D
+          font={'./fonts/Neuropol_Regular.json'}
+          bevelEnabled
+          bevelSize={0.05}
+          letterSpacing={0.1}
+          size={0.6}
+          scale={1.5}
+        >
+          {chosenOption}
+          <meshMatcapMaterial matcap={matcap} />
+        </Text3D>
+      </animated.mesh>
+    </Float>
+  );
+};
+
+export default ChosenOption;
